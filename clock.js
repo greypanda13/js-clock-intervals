44c3b8866d57e4e2f3d39ec909a4da7b00d6445d
greypanda13 <greypanda13@gmail.com>
2019-03-25 19:54:00 -0700

    functional clock after 59 seconds. before the 1-minute mark the hour hand moves erratically.

--- clock.js
+++ clock.js
@@ -1,73 +1,39 @@
 var hands = [
   {
-    name: "secs",
+    name: "second",
     elapsed: 0,
     unitsInParent: 60
   }, {
-    name: "mins",
+    name: "minute",
     elapsed: 0,
     unitsInParent: 60
   }, {
-    name: "hrs",
+    name: "hour",
     elapsed: 0,
     unitsInParent: 12
   }
 ];
-
-/*
-var secs = 0;
-var mins = 0;
-var hrs = 0;
 var degreesInCircle = 360;
-var secsPerMin = 60;
-var minsPerHr = 60;
-var hrsPerDay = 24;
-
-var secsRotateDeg = 6; // (360deg / 60 rotations)
-var minsRotateDeg = 6; // (360deg / 60 rotations)
-var hrsRotateDeg = 30; // (360deg / 12 rotations)
-*/
 
 function howManyDeg(whichHand) {
-  return ((degreesInCircle/hands[whichHand].unitsInParent) * hands[1].elapsed);
+  return ((degreesInCircle/hands[whichHand].unitsInParent) * hands[whichHand].elapsed);
 }
 function handRotate(whichHand) {
-
+  var handToRotate = document.getElementById(hands[whichHand].name);
+  console.log(hands[whichHand].name);
+  handToRotate.style.transform = "rotate(" + howManyDeg(whichHand) + "deg)";
 }
 
-
-
-
 setInterval(function(){
-  hands[0].elapsed++; // ++ work here? idk
+  hands[0].elapsed++;
+  console.log(hands[0].elapsed);
+  handRotate(0);
+
   for (var i = 1; i < hands.length; i++) {
     if (hands[i - 1].elapsed % hands[i - 1].unitsInParent === 0) {
       hands[i].elapsed++;
-      handRotate();
-      //DOM MANIP
+      // MANIPULATE THAT DOM BROOOOO
+      handRotate(i);
     }
   }
-  console.log("TICK!");
-
-
-    /*
-    //ADD TO SECONDS COUNTER, MAKE SECONDS HAND MOVE
-    hands[0].elapsed++; //not sure if ++ works here
-
-    //IF ANOTHER MINUTE ELAPSES, ADD TO MINUTES COUNTER, MAKE MINUTES HAND MOVE
-    if(hands[0].elapsed % secsPerMin === 0) {
-      hands[1].elapsed++; //not sure if ++ works here
-
-    }
-    //IF ANOTHER HOUR ELAPSES, ADD TO HOUR COUNTER, MAKE HOUR HAND MOVE
-    if(hands[1].elapsed % minsPerHr === 0) {
-      hands[2].elapsed++; //not sure if ++ works here
-
-    }
-    */
-
-    // var secsHand = document.getElementById("second");
-    // secsHand.style.transform = "rotate(" + secsRotateDeg + ")";
-      document.getElementById("minute").style.transform = "rotate(" + handRotate(mins, );
-    }
- }, 50);
+}, 50);
